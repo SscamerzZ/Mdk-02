ee8c8fd82531254a8ad8727c0a05781fdaa0b39b
Streltsov_Maxim <118896608+SscamerzZ@users.noreply.github.com>
2023-05-25 15:40:57 +0300

    Add files via upload

---
+++ Windows Forms Application 111/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Windows_Forms_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем был ли выбран файл
+            {
+                richTextBox1.Clear(); //Очищаем richTextBox
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt"; //Указываем что нас интересу только текстовые файлы
+                string fileName = openFileDialog1.FileName; //получаем наименование файл и путь к нему.
+                richTextBox1.Text = File.ReadAllText(fileName, Encoding.GetEncoding(1251)); //Передаем содержимое файла в richTextBox
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text Files|*.txt";//Задаем доступные расширения
+            saveFileDialog1.DefaultExt = ".txt"; //Задаем расширение по умолчанию 3
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем подтверждение сохранения информации.
+            {
+                var name = saveFileDialog1.FileName; //Задаем имя файлу File.WriteAllText(name, richTextBox.Text, Encoding.GetEncoding(1251)); //Записываем в файл содержимое textBox с кодировкой 1251
+            }
+            richTextBox1.Clear();
+        }
+
+        private void выравниваниеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Select(); // выравнивание только выделенного текста
+                                   //richTextBox1.SelectAll(); //выделение всего текста
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+            //richTextBox1.DeselectAll(); //Отмена выделения
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем был ли выбран файл
+            {
+                richTextBox1.Clear(); //Очищаем richTextBox
+                openFileDialog1.Filter = "Text Files (*.txt)|*.txt"; //Указываем что нас интересуют только текстовые файлы
+                string fileName = openFileDialog1.FileName; //получаем наименование файл и путь к нему.
+                richTextBox1.Text = File.ReadAllText(fileName, Encoding.GetEncoding(1251)); //Передаем содержимое файла в richTextBox
+            }
+
+        }
+
+        private void настройкаПринтераToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pageSetupDialog1.ShowDialog();
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (printDialog1.ShowDialog() == DialogResult.OK) printDocument1.Print();
+        }
+
+        private void предварительныйПросмотрToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text Files|*.txt";//Задаем доступные расширения
+            saveFileDialog1.DefaultExt = ".txt"; //Задаем расширение по умолчанию 3
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) //Проверяем подтверждение сохранения информации.
+            {
+                var name = saveFileDialog1.FileName; //Задаем имя файлу File.WriteAllText(name, richTextBox.Text, Encoding.GetEncoding(1251)); //Записываем в файл содержимое textBox с кодировкой 1251
+            }
+            richTextBox1.Clear();
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void вырезатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
